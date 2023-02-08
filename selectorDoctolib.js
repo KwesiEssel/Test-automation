cc5eeb590188d4e484ae9ad6a9742ca95126741c
KwesiEssel <44833655+KwesiEssel@users.noreply.github.com>
2023-02-08 13:58:36 +0100

    Create selectorDoctolib.js

---
+++ selectorDoctolib.js
@@ -0,0 +1,9 @@
+import { Selector } from 'testcafe'
+
+// button klassennamen
+export const userName = Selector('button[classenNamenTest]')
+// klassen vor den Namen einen punkt setzen
+export const hilfebereich = Selector('.classNamenHilfebereich')
+export const meineDokumente = Selector('.classNameDocuments')
+export const startseite = Selector('.classNameStartseite')
+
