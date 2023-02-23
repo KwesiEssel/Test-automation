18595f0d4eff03d0c46b9fa00f041e052edecf6e
KwesiEssel <44833655+KwesiEssel@users.noreply.github.com>
2023-02-23 12:24:37 +0100

    Update deleteBlocker.js

--- deleteBlocker.js
+++ deleteBlocker.js
@@ -1,7 +1,24 @@
-export const deleteBlockers = async (color) => {
-  await openCalendarTab()
+import { Selector, t } from 'testcafe';
+
+fixture('Delete Blockers Test')
+  .page('https://www.test.de');
+
+test('Delete blockers with color', async () => {
+  const calendarSelectors = {
+    iconDeleteAppointment: Selector('.appointment-context-menu').withText('Delete'),
+    buttonAllBlockersInTheSeries: Selector('.delete-recurring .delete-recurring-all'),
+  };
+  
+  const openCalendarTab = async () => {
+    // open calendar
+  };
+
+  const color = 'red';
+  
+  await openCalendarTab();
+  
   await t
-    .rightClick(Selector('div.color-' + color))
+    .rightClick(Selector(`div.color-${color}`))
     .click(calendarSelectors.iconDeleteAppointment)
-    .click(calendarSelectors.buttonAllBlockersInTheSeries)
-}
+    .click(calendarSelectors.buttonAllBlockersInTheSeries);
+});
