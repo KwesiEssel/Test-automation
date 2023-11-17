207c6e056f4ba3302444a5e7b248070c26c15d44
KwesiEssel <44833655+KwesiEssel@users.noreply.github.com>
2023-11-17 17:11:38 +0100

    ZahlungDefinition.groovy

---
+++ Java/src/main/groovy; gherkin; cucumber/pageobjects/ZahlungDefinition.groovy
@@ -0,0 +1,15 @@
+@Wenn ( der Kunde auf next-bahn.de den Gutschein Istringt einlöst )
+aDann(wird der Gutschein auf next-bahn.de {stringt eingelost!)
+public void der _Kunde_Loest_auf_next_bahn_de den _Gutschein_ein(String gutscheincode) {
+der text = "Der Wert Ihrer Buchung ist durch Ihren Gutschein komplett abgedeckt
+Zahlung.gutscheinHinzufuegenBundeswehr_cick()
+Zahlung .gutscheinFeldBundeswehr.waitUntilisVisible().setText(gutscheincode)
+Zahlung.gutscheinHinzufuegenBundeswehn.cLick()
+Zahlung.gutscheinContent.vaitForText(text)
+}
+ZahLung_gutscheinContent.assentContainsText(text)
+Dann('wird der Kunde auf next-bahn.de "Jetzt kaufen' auswählen)
+@Wenn('der Kunde auf next-bahn.de "Jetzt kaufen" auswant)
+public void der_Kunde_auf_next_bahn_de_jetzt_kaufen_auswaehlt() {
+Zahlung.btnJetztKaufen.click()
+}
