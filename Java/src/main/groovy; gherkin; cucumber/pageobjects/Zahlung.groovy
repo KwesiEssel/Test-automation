68c586a7a88239212546325d491b87549b456250
KwesiEssel <44833655+KwesiEssel@users.noreply.github.com>
2023-11-17 17:07:09 +0100

    Zahlung.groovy

---
+++ Java/src/main/groovy; gherkin; cucumber/pageobjects/Zahlung.groovy
@@ -0,0 +1,3 @@
+static gutscheinFeld = new TestElement(By.id('zahlunggutschein')
+static button = new TestElement(By.id('weiter')
+static gutscheinHinzufuegen = new TestElement(By.id('gutscheinhinzufuegen')
