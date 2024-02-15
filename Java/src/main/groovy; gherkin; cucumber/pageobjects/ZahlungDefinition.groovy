6945c8de6718f1a38f1d4a2230ee37d688e5ccef
KwesiEssel <44833655+KwesiEssel@users.noreply.github.com>
2024-02-15 05:07:10 +0100

    Update ZahlungDefinition.groovy

--- Java/src/main/groovy; gherkin; cucumber/pageobjects/ZahlungDefinition.groovy
+++ Java/src/main/groovy; gherkin; cucumber/pageobjects/ZahlungDefinition.groovy
@@ -1,6 +1,6 @@
-@Wenn ( der Kunde auf next-bahn.de den Gutschein Istringt einlöst )
+@Wenn ( der Kunde auf den Gutschein {string} einlöst )
 @Dann(wird der Gutschein auf next-bahn.de {stringt eingelost!)
-public void der _Kunde_Loest_auf_next_bahn_de den _Gutschein_ein(String gutscheincode) {
+public void der _Kunde_Loest den _Gutschein_ein(String gutscheincode) {
 der text = "Der Wert Ihrer Buchung ist durch Ihren Gutschein komplett abgedeckt
 Zahlung.gutscheinHinzufuegenBundeswehr_cick()
 Zahlung .gutscheinFeldBundeswehr.waitUntilisVisible().setText(gutscheincode)
