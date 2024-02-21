22ea08c86f1b5c8b3f2b819717d5eb6ab40f3124
KwesiEssel <dadzie_essel@hotmail.de>
2024-02-21 14:57:21 +0100

    PermutationsGenerator

---
+++ src/PermutationsGenerator.java
@@ -0,0 +1,34 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PermutationsGenerator {
+
+    public static void main(String[] args) {
+        String s = "";
+        List<String> permutations = generatePermutations(s);
+        for (String perm : permutations) {
+            System.out.println(perm);
+        }
+    }
+
+    public static List<String> generatePermutations(String s) {
+        List<String> result = new ArrayList<>();
+        permuteHelper("", s, result);
+        return result;
+    }
+
+    private static void permuteHelper(String prefix, String remainder, List<String> result) {
+        if (remainder.length() == 0) {
+            result.add(prefix);
+        } else {
+            for (int i = 0; i < remainder.length(); i++) {
+                // Neuer Prefix wird um das aktuelle Zeichen erweitert
+                String newPrefix = prefix + remainder.charAt(i);
+                // Erstelle einen neuen Reststring ohne das aktuelle Zeichen
+                String newRemainder = remainder.substring(0, i) + remainder.substring(i + 1);
+                permuteHelper(newPrefix, newRemainder, result);
+            }
+        }
+    }
+}
---
+++ src/Wortsammlung.java
@@ -0,0 +1,33 @@
+import java.io.*;
+
+public class Wortsammlung {
+
+    // Pfad zur Datei
+    private String filePath = "C:\\Users\\dadzi\\Documents\\PAR GmbH\\Wortsammlung.txt"; // Beispiel-Pfad
+
+    public Wortsammlung() throws IOException {
+        readFile();
+    }
+
+    private void readFile() throws IOException {
+        File file = new File(filePath);
+        BufferedReader reader = null;
+
+        try {
+            reader = new BufferedReader(new FileReader(file));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } finally {
+            if (reader != null) {
+                reader.close(); // Sicherstellen, dass der Reader auch bei einem Fehler geschlossen wird
+            }
+        }
+    }
+
+    /* @Override
+    public String toString() {
+        return "Ordner:     " + filePath;
+    } */
+}
